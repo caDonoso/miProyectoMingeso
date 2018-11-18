52e78f2e563ab08575147cd50e4027c9f0b97c1f
Cristobal Donoso <cristobal.donoso.s@usach.cl>
2018-11-18 12:48:37 -0300

    addHelloWorldEntitie
    se crea la clase hello que imprime Hello World por pantalla.

---
+++ grupoPersonal/backend/src/main/java/entities/hello.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class hello {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
